51a6a4fc5d13f2e531d7aff4a5b36a4d0a0681e7
= <=>
2024-11-27 15:48:40 -0300

    Altered Escape is mine now

---
+++ elec/main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true
+    },
+    title: 'My App'
+  });
+
+  win.setTitle('My App');
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
